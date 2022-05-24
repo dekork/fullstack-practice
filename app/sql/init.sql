ae5497f5cb88b4197fa431a813ea4221f8a58947
dekork <deniskrkt@gmail.com>
2022-05-23 17:54:29 -0700

    fix tables names

--- app/sql/init.sql
+++ app/sql/init.sql
@@ -1,10 +1,10 @@
-CREATE TABLE practice (
+CREATE TABLE workouts (
   id int NOT NULL AUTO_INCREMENT,
   workout_name VARCHAR(255) NOT NULL,
   Date DATE NOT NULL,
   PRIMARY KEY (id)
 );
 INSERT INTO
-  practice (workout_name, Date)
+  workouts (workout_name, Date)
 VALUES
   ("Abs", CURRENT_DATE());
